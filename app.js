e34352d2d4270abf5305c76f69d073942560324b
Ash <154726037+aarij0119@users.noreply.github.com>
2024-12-01 22:19:10 +0530

    add-cors

--- app.js
+++ app.js
@@ -1,54 +1,50 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const app = express();
 const path = require('path');
 const multer = require('multer');
 const crypto = require('crypto');
 const fs = require('fs');
-require('dotenv').config()
-const PORT = process.env.PORT || 3000
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
 
+// Use CORS middleware
 app.use(cors());
 
 app.set("view engine", 'ejs');
-
 app.use(express.static(path.join(__dirname, "public")));
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './public/images/uploads')
+        cb(null, './public/images/uploads');
     },
     filename: function (req, file, cb) {
         crypto.randomBytes(10, function (err, bytes) {
-            if (err) return cb(err)
-            console.log(bytes)
-            const fn = bytes.toString("hex") + path.extname(file.originalname)
-            console.log(fn)
-            cb(null, fn)
-        })
+            if (err) return cb(err);
+            const fn = bytes.toString("hex") + path.extname(file.originalname);
+            cb(null, fn);
+        });
     }
-})
-const upload = multer({ storage: storage })
-
+});
+const upload = multer({ storage: storage });
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index');
 });
 
 app.post('/uploads', upload.single("image"), (req, res) => {
-    console.log(req.file)
-    res.send("done")
+    res.send("done");
 });
 
 app.get('/images', (req, res) => {
     fs.readdir('./public/images/uploads', (err, files) => {
         if (err) {
-            return res.send("unable to read files")
+            return res.send("unable to read files");
         }
-        const imageurl = files.map(file => `http://localhost:3000/images/uploads/${file}`)
+        const imageurl = files.map(file => `https://${req.headers.host}/images/uploads/${file}`);
         res.json(imageurl);
-    })
-})
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
